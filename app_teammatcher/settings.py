b1fa17945b9d864f94c941814fd31da6f43aa270
Rasitha Hewage <121127737+rasithahewage@users.noreply.github.com>
2026-02-27 16:12:17 +0100

    Update settings.py- new update static

--- app_teammatcher/settings.py
+++ app_teammatcher/settings.py
@@ -94,12 +94,16 @@ USE_TZ = True
 
 
 
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles'
+STATICFILES_DIRS = []
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
+#STATIC_URL = 'static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, 'static'),
+#]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
